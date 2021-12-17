7962cc37da75a2c9cf80c6789f399b43598d314b
Tien Nguyen <tien.nguyenanh@alta.com.vn>
2021-12-18 02:13:20 +0700

    add basic Player movement

---
+++ Assets/Scripts/Character/MouseController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    [Header("The Character")]
+    [SerializeField] GameObject thePlayer;
+
+    [Header("Sensitivity Setting")]
+    [SerializeField] float X_sensitivity = 100f;
+    [SerializeField] float Y_sensitivity = 100f;
+
+    [Header("Looking Range Setting")]
+    [SerializeField] float maximumX = 60f;
+    [SerializeField] float minimumX = -60f;
+
+    float rotationX;
+
+    public static event Action<float> MouseRotate;
+
+    public bool IsCanRotate
+    {
+        get;
+        set;
+    }
+
+    private void Start()
+    {
+        IsCanRotate = true;
+
+        //Hide and lock the cursor in center screen
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if (IsCanRotate)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * X_sensitivity * Time.deltaTime;
+
+            rotationX += Input.GetAxis("Mouse Y") * Y_sensitivity * Time.deltaTime;
+            rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+
+            //Look Up and down with mouse Y input
+            transform.localRotation = Quaternion.Euler(-rotationX, 0, 0);
+
+            //Rotate The Player with mouse X input
+            thePlayer.transform.Rotate(Vector3.up * mouseX);
+        }
+    }
+}
---
+++ Assets/Scripts/Character/MovingController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [SerializeField] float speed = 10f;
+    [SerializeField] float jumpPower = 10f;
+
+    [Header("Animator")]
+    [SerializeField] Animator anim;
+
+    float gravityScale = 5;
+
+    bool isGrounded;
+    RaycastHit hit;
+    float distanceCheckGround = 1.5f;
+
+    public bool IsCanMove
+    {
+        get;
+        set;
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        IsCanMove = true;
+    }
+
+    private void Update()
+    {
+        if (IsCanMove)
+        {
+            float h_input = Input.GetAxis("Horizontal");
+            float v_input = Input.GetAxis("Vertical");
+
+            //Get any value of horizontal || vertical input => Move it
+            if (h_input != 0 || v_input != 0)
+            {
+                transform.Translate(new Vector3(h_input, 0, v_input) * speed * Time.deltaTime);
+            }
+
+            //Check ground
+            Ray ray = new Ray(transform.position, Vector3.down);
+            isGrounded = Physics.Raycast(ray, out hit, distanceCheckGround);
+            //Debug.DrawRay(transform.position, Vector3.down * distanceCheckGround, Color.red);
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            }
+
+            anim.SetBool("IsRunning", v_input > 0 && isGrounded);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(Physics.gravity * (gravityScale - 1) * rb.mass);
+    }
+
+
+}
